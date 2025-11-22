d5e7142c59e588f4a7a24b4bd13d67e3ce6640fa
Yeakubkhan <mdyeakubkhan768@gmail.com>
2025-11-22 14:35:00 +0600

    new

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    uploaded_at = models.DateTimeField(auto_now_add=True,null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    
+
+    def __str__(self):
+        return f"{self.product.name} Image"
