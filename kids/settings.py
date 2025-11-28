9dd0a93c13c368b8ad645c20ab383903a249fc25
Yeakubkhan <mdyeakubkhan768@gmail.com>
2025-11-28 08:53:23 +0600

    new

--- kids/settings.py
+++ kids/settings.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 import os
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
+# ----------------------- #
+#   BASE DIRECTORY
+# ----------------------- #
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ----------------------- #
 #   SECURITY & DEBUG
 # ----------------------- #
-
 SECRET_KEY = 'django-insecure-!your-secret-key-here!'
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-
 
 CSRF_TRUSTED_ORIGINS = [
     "https://kidsitemshop-production.up.railway.app",
@@ -20,7 +24,6 @@ CSRF_TRUSTED_ORIGINS = [
 # ----------------------- #
 #   INSTALLED APPS
 # ----------------------- #
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -34,9 +37,8 @@ INSTALLED_APPS = [
 
     # S3 storage
     'storages',
-]
 
-INSTALLED_APPS += [
+    # Cloudinary
     "cloudinary",
     "cloudinary_storage",
 ]
@@ -44,7 +46,6 @@ INSTALLED_APPS += [
 # ----------------------- #
 #   MIDDLEWARE
 # ----------------------- #
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -62,7 +63,6 @@ ROOT_URLCONF = 'kids.urls'
 # ----------------------- #
 #   TEMPLATES
 # ----------------------- #
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -83,7 +83,6 @@ WSGI_APPLICATION = 'kids.wsgi.application'
 # ----------------------- #
 #   DATABASES
 # ----------------------- #
-
 DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR}/db.sqlite3",
@@ -94,7 +93,6 @@ DATABASES = {
 # ----------------------- #
 #   PASSWORD VALIDATION
 # ----------------------- #
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -103,48 +101,45 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # ----------------------- #
-#   TIME & LANG
+#   TIME & LANGUAGE
 # ----------------------- #
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-
-
 # ----------------------- #
 #   STATIC FILES
 # ----------------------- #
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
-
-
 # ----------------------- #
-#   MEDIA FILES (S3)
+#   MEDIA FILES
 # ----------------------- #
-
-# Use S3 in production, fallback to local media in dev
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
 # ----------------------- #
-#   CLOUDINARY SETTINGS 
+#   CLOUDINARY SETTINGS
 # ----------------------- #
+cloudinary.config(
+    cloud_name="dlfr1kfqa",
+    api_key="525535723275894",
+    api_secret="l7SGe7cpOszd82aK8h8AAxfGHBg"
+)
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": 'dlfr1kfqa',
-    "API_KEY": '525535723275894',
-    "API_SECRET": 'l7SGe7cpOszd82aK8h8AAxfGHBg',
+    "CLOUD_NAME": "dlfr1kfqa",
+    "API_KEY": "525535723275894",
+    "API_SECRET": "l7SGe7cpOszd82aK8h8AAxfGHBg",
 }
+
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
+# ----------------------- #
+#   DEFAULT AUTO FIELD
+# ----------------------- #
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
