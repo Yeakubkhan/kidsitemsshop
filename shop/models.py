fd858e8e05bf9366c5e1a77636bac9474b63bdfc
Yeakubkhan <mdyeakubkhan768@gmail.com>
2025-12-04 11:21:38 +0600

    new change

--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     color=models.CharField(max_length=100, blank=True)
     size = models.CharField(max_length=50)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     uploaded_at = models.DateTimeField(auto_now_add=True,null=True, blank=True)
 
